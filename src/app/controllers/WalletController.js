ceba59171b440863c890cb1aa13862334412436a
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-03 22:17:30 -0300

    Created update method.

--- src/app/controllers/WalletController.js
+++ src/app/controllers/WalletController.js
@@ -12,7 +12,9 @@ class WalletController {
     const user = await User.findByPk(userId);
 
     if (!user) {
-      return res.status(401).json({ message: 'Invalid inputs' });
+      return res
+        .status(401)
+        .json({ message: `User with ID ${userId} was not found` });
     }
 
     const wallet = await Wallet.create({
@@ -23,9 +25,35 @@ class WalletController {
     return res.json({ wallet });
   }
 
-  async update(req, res) {}
+  async update(req, res) {
+    const { description } = req.body;
 
-  async get(req, res) {}
+    if (!description) {
+      return res
+        .status(401)
+        .json({ message: 'Could not update description to null' });
+    }
+  }
+
+  async get(req, res) {
+    const { userId } = req.params;
+
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: `User with ID ${userId} was not found` });
+    }
+
+    const wallets = await Wallet.findAll({
+      where: {
+        userId
+      }
+    });
+
+    return res.json(wallets.toJSON());
+  }
 
   async delete(req, res) {}
 }
