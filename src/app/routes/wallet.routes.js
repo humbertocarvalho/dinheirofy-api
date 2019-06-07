01cda48d06b93b35382d4628b3464bd879993c79
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-06 22:13:46 -0300

    Changed delete route to accept parameters.

--- src/app/routes/wallet.routes.js
+++ src/app/routes/wallet.routes.js
@@ -4,6 +4,6 @@ const WalletController = require('../controllers/WalletController');
 routes.get('/wallet/:userId', WalletController.get);
 routes.post('/wallet', WalletController.create);
 routes.put('/wallet/:walletId', WalletController.update);
-routes.delete('/wallet', WalletController.delete);
+routes.delete('/wallet/:walletId', WalletController.delete);
 
 module.exports = routes;
