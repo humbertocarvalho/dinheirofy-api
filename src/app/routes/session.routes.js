66752e30fefd9afa23388cc7d332fdae02b1130a
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-10 21:01:49 -0300

    Started new nestjs project.

--- src/app/routes/session.routes.js
+++
@@ -1,5 +0,0 @@
-const routes = require('express').Router();
-const SessionController = require('../controllers/SessionController');
-routes.post('/session/auth', SessionController.auth);
-
-module.exports = routes;
