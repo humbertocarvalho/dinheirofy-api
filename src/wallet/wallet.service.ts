790a3bc489d8e9a927e0a0f31ef81b34ac5a7b6a
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-22 18:36:35 -0300

    Add put, delete and get methods.

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UserService } from '../user/user.service';
 import { IWallet } from './interfaces/wallet.interface';
+import { UpdateWalletDto } from './dto/update-wallet.dto';
 
 @Injectable()
 export class WalletService {
@@ -20,5 +21,17 @@ export class WalletService {
       user: await this.userService.findById(createWalletDto.userId),
     };
     return this.walletRepository.save(wallet);
+  }
+
+  async update(walletId: number, updateWalletDTO: UpdateWalletDto) {
+    return this.walletRepository.update({ id: walletId }, updateWalletDTO);
+  }
+
+  async delete(walletId: number) {
+    return this.walletRepository.delete({ id: walletId });
+  }
+
+  async get() {
+    return this.walletRepository.find();
   }
 }
